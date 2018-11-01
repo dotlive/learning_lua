7849240793ea2b2fe7e87819466d77238539630d
dotlive <dotlive@163.com>
2018-11-01 11:09:34 +0800

    解决无法加载自定义C库的问题;

--- src/lua-5.2.2/mylib/test.lua
+++ src/lua-5.2.2/mylib/test.lua
@@ -1,19 +1,24 @@
 ---[[
 -- loadlib
-local entry = package.loadlib("mylib.dll", "luaopen_mylib")
-if entry then
-	entry()
-	print(entry().add(1,2))
+local entry_pb = package.loadlib("lpblib.dll", "luaopen_pb")
+if entry_pb then
+	local pb = entry_pb()
+	if not pb then
+		return
+	end
+
+	print("success")
+
 else
 	print("failed")
 end
 --]]
 
---[[
+---[[
 -- require
 local entry = require("mylib")
 if entry then
-	print(entry.add(1,2))
+	print(entry.add(1, 2))
 else
 	print("failed")
 end
